1dfd93430ea2ecab89ff9a4862795f83500a15f5
dutsn <giihrapozinha13@gmail.com>
2026-06-05 04:39:53 -0300

    05-06-2026

---
+++ Jogo2D/Assets/Scripts/PersonagemController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonagemController : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float vel;
+
+    void Start()
+    {
+        rb2d = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        if(rb2d.velocity.magnitude<5){
+            rb2d.velocity += new Vector2(vel,0)*horizontalInput*Time.deltaTime;
+        }
+    }
+}
