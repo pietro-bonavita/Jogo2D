78c12bc945a287d65e60f1caeb5358f7ef538b52
pietro-bonavita <pcastrobonavita@gmail.com>
2026-07-03 08:19:02 -0300

    03/07

--- Jogo2D/Assets/CoinController.cs
+++ Jogo2D/Assets/CoinController.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,13 @@ public class CoinController : MonoBehaviour
 {
     public CircleCollider2D Collider2D;
     public PersonagemController  personagemController;
+    public GameObject coinParticle;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             personagemController.AddToPoints(1);
+            Instantiate(coinParticle, this.transform.position, UnityEngine.Quaternion.Euler(-90,0,0));
             Destroy(this.gameObject);
             
         }
